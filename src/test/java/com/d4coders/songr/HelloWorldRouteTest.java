2f74d8c2323e809c9005ede8c4ad227f5ced22f8
DoaaDaban <doaadaban993@gmail.com>
2021-10-12 23:50:31 +0300

    addTest

--- src/test/java/com/d4coders/songr/HelloWorldRouteTest.java
+++ src/test/java/com/d4coders/songr/HelloWorldRouteTest.java
@@ -8,11 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,16 +28,5 @@ class HelloWorldRouteTest {
         assertThat(mainControllerTest).isNotNull();
     }
 
-    @Test
-    public void testGreetingReturnData() throws Exception{
-        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/hello",
-                String.class)).contains("Hello world");
-    }
-
-    @Test
-    public void testRequestStatusAndReturn() throws Exception{
-        this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello world")));
-    }
 }
 
