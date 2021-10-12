daf550d9ccfff7fbe6463713a9ecb1d13f2ab6ef
DoaaDaban <doaadaban993@gmail.com>
2021-10-12 23:51:28 +0300

    addTest

--- src/test/java/com/d4coders/songr/HelloWorldRouteTest.java
+++ src/test/java/com/d4coders/songr/HelloWorldRouteTest.java
@@ -5,16 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.web.servlet.MockMvc;
+
 
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class HelloWorldRouteTest {
-  
+
     @Autowired
     MainController mainControllerTest;
 
